cb578ea2727887011bbcafdddafdcf425d44cf98
chenjxJava <1114211383@qq.com>
2026-02-26 11:22:54 +0800

    feat: 初始化基于Spring Cloud的空服务骨架

---
+++ src/test/java/com/example/emptyservice/EmptyServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.emptyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
